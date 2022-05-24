021a745bb472b1b34d8fd4691b9893802e61837d
JamesCostello-dev <jcotab@gmail.com>
2022-05-23 18:45:47 -0700

    best time to sell and buy a stock

---
+++ array/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+#Best Time to Buy and Sell Stock II
+def maxProfit(prices):
+    if len(prices) == 0:
+        return 0
+    max_profit = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            max_profit += prices[i] - prices[i - 1]
+    return max_profit
+
+def test_max_profit():
+    assert maxProfit([7, 1, 5, 3, 6, 4]) == 7
+    assert maxProfit([1, 2, 3, 4, 5]) == 4
+    assert maxProfit([7, 6, 4, 3, 1]) == 0
+
+#time complexity: O(n)
+#space complexity: O(1)
+
+if __name__ == '__main__':
+    test_max_profit()
--- array/remove-dupes.py
+++ array/remove-dupes.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 #remove duplicates from an array
-def removeDuplicates(self, nums: List[int]) -> int:
+def removeDuplicates(nums):
     if len(nums) == 0:
         return 0
     i = 0
@@ -10,12 +12,12 @@ def removeDuplicates(self, nums: List[int]) -> int:
     return i + 1
 
 def test_remove_duplicates():
-    nums = [1, 1, 2]
-    assert removeDuplicates(nums) == 2
-    nums = [0,0,1,1,1,2,2,3,3,4]
-    assert removeDuplicates(nums) == 5
-    nums = [1,1,1,2,2,3]
-    assert removeDuplicates(nums) == 4
+    assert removeDuplicates([1, 1, 2]) == 2 
+    assert removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
+    assert removeDuplicates([]) == 0
 
 #time complexity: O(n)
 #space complexity: O(1)
+
+if __name__ == '__main__':
+    test_remove_duplicates()
